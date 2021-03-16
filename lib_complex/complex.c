0dc3db38d32474950cd6ad0e942083a425c657f4
lblopes <lblopes@dcc.fc.up.pt>
2021-03-16 13:03:24 +0000

    correction in complex_div

--- lib_complex/complex.c
+++ lib_complex/complex.c
@@ -35,8 +35,8 @@ complex complex_mul(complex z, complex w) {
 complex complex_div(complex z, complex w) {
   complex r;
   double d = w.x * w.x + w.y * w.y;
-  r.x = (z.x * w.x + z.y * w.y) / d;
-  r.y = (- z.x * w.y + z.y + w.x) / d;
+  r.x = (  z.x * w.x + z.y * w.y) / d;
+  r.y = (- z.x * w.y + z.y * w.x) / d;
   return r;
 }
 
